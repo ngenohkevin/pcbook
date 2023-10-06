9e1f808da9846ca82e36460181559129a3f9f181
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-06 11:00:03 +0300

    new float32 function

--- sample/random.go
+++ sample/random.go
@@ -65,6 +65,9 @@ func randomInt(min, max int) int {
 func randomFloat64(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
+func randomFloat32(min, max float32) float32 {
+	return min + rand.Float32()*(max-min)
+}
 
 func randomStringFromSet(a ...string) string {
 	n := len(a)
