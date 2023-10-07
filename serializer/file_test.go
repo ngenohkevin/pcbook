49ee394fcc0d266a2c4b7c5c9f14400e169cfa38
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-07 12:48:23 +0300

    test binary file

--- serializer/file_test.go
+++ serializer/file_test.go
@@ -1,7 +1,10 @@
 package serializer
 
 import (
+	"github.com/golang/protobuf/proto"
+	"github.com/ngenohkevin/pcbook/pb"
 	"github.com/ngenohkevin/pcbook/sample"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -12,5 +15,10 @@ func TestFileSerializer(t *testing.T) {
 
 	laptop1 := sample.NewLaptop()
 	err := WriteProtobufToBinaryFile(laptop1, binaryFile)
+	require.NoError(t, err)
 
+	laptop2 := &pb.Laptop{}
+	err = ReadProtobuffFromBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err)
+	require.True(t, proto.Equal(laptop1, laptop2))
 }
