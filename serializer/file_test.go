76f60f5449d194588aed5d69697b92e7703609a3
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-07 12:37:43 +0300

    Test file serializer

--- serializer/file_test.go
+++ serializer/file_test.go
@@ -1,7 +1,16 @@
 package serializer
 
-import "testing"
+import (
+	"github.com/ngenohkevin/pcbook/sample"
+	"testing"
+)
 
 func TestFileSerializer(t *testing.T) {
+	t.Parallel()
+
+	binaryFile := "../tmp/laptop.bin"
+
+	laptop1 := sample.NewLaptop()
+	err := WriteProtobufToBinaryFile(laptop1, binaryFile)
 
 }
