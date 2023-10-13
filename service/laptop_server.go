8da2d37b49c2f9468d2ce2518700f1f23e591c2d
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-13 13:53:35 +0300

    create laptop

--- service/laptop_server.go
+++ service/laptop_server.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"github.com/ngenohkevin/pcbook/pb"
+	"log"
 )
 
 type LaptopServer struct {
@@ -14,5 +15,6 @@ func NewLaptopServer() *LaptopServer {
 
 // CreateLaptop is a unary RPC to create a new laptop
 func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
-
+	laptop := req.GetLaptop()
+	log.Printf("recieve a create-laptop request with id: %s", laptop.Id)
 }
