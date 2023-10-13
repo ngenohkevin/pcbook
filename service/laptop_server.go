5c3baea706c7333ccfb60c24f586fd535bdb72cf
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-13 13:52:07 +0300

    create laptop

--- service/laptop_server.go
+++ service/laptop_server.go
@@ -12,6 +12,7 @@ func NewLaptopServer() *LaptopServer {
 	return &LaptopServer{}
 }
 
+// CreateLaptop is a unary RPC to create a new laptop
 func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
 
 }
