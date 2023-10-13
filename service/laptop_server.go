f551fb07a743bdb725249def87bb87b5e4d85197
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-13 13:51:34 +0300

    create laptop

--- service/laptop_server.go
+++ service/laptop_server.go
@@ -1,8 +1,17 @@
 package service
 
+import (
+	"context"
+	"github.com/ngenohkevin/pcbook/pb"
+)
+
 type LaptopServer struct {
 }
 
 func NewLaptopServer() *LaptopServer {
 	return &LaptopServer{}
+}
+
+func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
+
 }
