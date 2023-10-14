78c3feb3b2fbbccc89fed7e0a09e633153d7699f
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-14 13:18:19 +0300

    laptop test cases

--- service/laptop_server_test.go
+++ service/laptop_server_test.go
@@ -3,12 +3,23 @@ package service
 import (
 	"github.com/ngenohkevin/pcbook/pb"
 	"github.com/ngenohkevin/pcbook/sample"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"testing"
 )
 
 func TestServerCreateLaptop(t *testing.T) {
 	t.Parallel()
+	laptopNoID := sample.NewLaptop()
+	laptopNoID.Id = ""
+
+	laptopInvalidID := sample.NewLaptop()
+	laptopInvalidID.Id = "invalid-uuid"
+
+	laptopDuplicateID := sample.NewLaptop()
+	storeDuplicateID := NewInMemoryLaptopStore()
+	err := storeDuplicateID.Save(laptopDuplicateID)
+	require.Nil(t, err)
 
 	testCases := []struct {
 		name   string
@@ -19,6 +30,34 @@ func TestServerCreateLaptop(t *testing.T) {
 		{
 			name:   "success_with_id",
 			laptop: sample.NewLaptop(),
+			store:  NewInMemoryLaptopStore(),
+			code:   codes.OK,
 		},
+		{
+			name:   "success_no_id",
+			laptop: laptopNoID,
+			store:  NewInMemoryLaptopStore(),
+			code:   codes.OK,
+		},
+		{
+			name:   "failure_invalid_id",
+			laptop: laptopInvalidID,
+			store:  NewInMemoryLaptopStore(),
+			code:   codes.InvalidArgument,
+		},
+		{
+			name:   "failure_duplicate_id",
+			laptop: laptopDuplicateID,
+			store:  storeDuplicateID,
+			code:   codes.AlreadyExists,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+		})
 	}
 }
