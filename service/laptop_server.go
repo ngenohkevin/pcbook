a4104d6360a5b48eb357505bd110618ec22d631d
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-14 12:33:00 +0300

    create laptop request

--- service/laptop_server.go
+++ service/laptop_server.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/ngenohkevin/pcbook/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 )
 
@@ -17,4 +20,18 @@ func NewLaptopServer() *LaptopServer {
 func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
 	laptop := req.GetLaptop()
 	log.Printf("recieve a create-laptop request with id: %s", laptop.Id)
+
+	if len(laptop.Id) > 0 {
+		//	check if it's a valid UUID
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop ID is not a valid UUID: %v", err)
+		} else {
+			id, err := uuid.NewRandom()
+			if err != nil {
+				return nil, status.Errorf(codes.Internal, "cannot generate anew laptop ID: %v", err)
+			}
+			laptop.Id = id.String()
+		}
+	}
 }
