2a5cc4906ff1ecf77d809b333f4a8201a8e5e360
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-19 11:48:16 +0300

    starting server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/ngenohkevin/pcbook/pb"
+	"github.com/ngenohkevin/pcbook/service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("hello server")
+	port := flag.Int("port", 0, "the server port")
+	flag.Parse()
+	log.Printf("start server on port %d", *port)
+
+	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	address := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
 }
